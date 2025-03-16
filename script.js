9fd1469d0abe15441bd8e847ecb5e2623bb0873e
LilMic1 <87609234+LilMic1@users.noreply.github.com>
2025-03-16 11:31:42 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tradeButton = document.querySelector(".trade-button");
+    tradeButton.addEventListener("click", function() {
+        alert("Redirecting to trade platform...");
+    });
+});
